59d863886d237279e4b6144462edc52ff1a2c2f5
anulisba <anulisba@gmail.com>
2025-03-26 14:42:58 +0530

    sidebar trigger

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -271,7 +271,7 @@ const SidebarTrigger = React.forwardRef<
       data-sidebar="trigger"
       variant="ghost"
       size="icon"
-      className={cn("h-7 w-7 py-4 mt-3", className)}
+      className={cn("h-7 w-7 py-4 mt-6", className)}
       onClick={(event) => {
         onClick?.(event);
         toggleSidebar();
@@ -615,7 +615,7 @@ const SidebarMenuAction = React.forwardRef<
         "peer-data-[size=lg]/menu-button:top-2.5",
         "group-data-[collapsible=icon]:hidden",
         showOnHover &&
-          "group-focus-within/menu-item:opacity-100 group-hover/menu-item:opacity-100 data-[state=open]:opacity-100 peer-data-[active=true]/menu-button:text-sidebar-accent-foreground md:opacity-0",
+        "group-focus-within/menu-item:opacity-100 group-hover/menu-item:opacity-100 data-[state=open]:opacity-100 peer-data-[active=true]/menu-button:text-sidebar-accent-foreground md:opacity-0",
         className
       )}
       {...props}
